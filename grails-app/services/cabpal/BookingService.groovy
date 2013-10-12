3ad2f63a41c2228b23315464a7271b4abf0d2d13
Jagdeep Nagpal <jagdeep.nagpal@bskyb.com>
2013-10-12 17:10:11 +0100

    added lib

--- grails-app/services/cabpal/BookingService.groovy
+++ grails-app/services/cabpal/BookingService.groovy
@@ -5,9 +5,17 @@ import grails.transaction.Transactional
 @Transactional
 class BookingService {
 
+    /** The Constant ACCOUNT_SID. Find it at twilio.com/user/account */
+    public static final String ACCOUNT_SID = "ACb9ddd21a0383212ec56b2d209d819275"
+
+    /** The Constant AUTH_TOKEN. Find it at twilio.com/user/account */
+    public static final String AUTH_TOKEN = "a263dd37812b0ce7259142e589af20a2"
+
     def clientBooking(params) {
         Booking booking = new Booking(params)
         //Trillio call comes here.
+
+
 
 
     }
