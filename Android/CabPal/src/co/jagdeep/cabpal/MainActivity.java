47787c607b7e9b280e493648015d86cde419fa82
M Mizan <m.j.mizan@gmail.com>
2013-10-13 02:10:57 +0100

    Working yey

--- Android/CabPal/src/co/jagdeep/cabpal/MainActivity.java
+++ Android/CabPal/src/co/jagdeep/cabpal/MainActivity.java
@@ -5,33 +5,55 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
+	ImageButton imageButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		addListenerOnButton();
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
-    @Override
-    public boolean onMenuItemSelected(int featureId, MenuItem item) {
-    	// TODO Auto-generated method stub
-        switch (item.getItemId()) {
-        case R.id.action_settings:
-        	Intent intent = new Intent(this, SettingsActivity.class);
-        	startActivity(intent);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		// TODO Auto-generated method stub
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			Intent intent = new Intent(this, SettingsActivity.class);
+			startActivity(intent);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void addListenerOnButton() {
+
+		imageButton = (ImageButton) findViewById(R.id.imageButton1);
+
+		imageButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+
+				imageButton.setImageResource(R.drawable.ic_launcher2); 
+
+			}
+
+		});
+
+	}
+
 }
